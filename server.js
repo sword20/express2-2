f7d8c6c44742d92cfe8106f8b10979385329b8a9
sword20 <72461412+sword20@users.noreply.github.com>
2021-03-12 22:56:12 -0800

    fixes server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.set('view engine','ejs');//sent views to EJS
 //this is how a route is built in Express
 app.get('/', (req,res)=>{
   var title = "Our Home Page";
-  res.render('index',{title:title});
+  res.render('pages/index',{title:title});
 });
 
 
